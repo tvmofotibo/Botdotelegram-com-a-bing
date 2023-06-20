5984780838900310b9a98de81af3a7256d46611c
tvmofotibo <73025025+tvmofotibo@users.noreply.github.com>
2023-06-20 16:42:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
-
 def start():
-
+    os.system("python  server.py > output.txt")
     return 'BOT ONLINE'
 
 if __name__ == '__main__':
-
-    app.run()
+    app.run(host='0.0.0.0')
